1e7bb1b0215e621d8592430e4b2caff75bdb03d4
chanwoosung <chanwoosung@naver.com>
2022-05-19 13:00:26 +0900

    image error fix

--- src/component/footernavbar.js
+++ src/component/footernavbar.js
@@ -1,9 +1,14 @@
 import css from '../styles/footernavbar.module.css'
+
 export default function FooterNavBar() {
     return(
         <div className={css.board}>
-            <div></div>
-            <div></div>
+            <div>
+                <img className={css.button} src="img/btn_home_select.png" alt='homebutton'/>
+            </div>
+            <div>
+                <img className={css.button} src="img/btn_Sheet.png" alt='scrapbutton'/>
+            </div>
         </div>
     )
 }
